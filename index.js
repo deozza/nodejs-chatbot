a87ea9e25226589b811d1fa88d036b9eb94c7516
Edenn Touitou <deozza@gmail.com>
2020-09-29 16:37:53 +0200

    added procfile

--- index.js
+++ index.js
@@ -5,7 +5,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 
-const PORT = 3000;
+const PORT = process.env.PORT || 5000;
 
 let app = express();
 app.use(bodyParser.json());
