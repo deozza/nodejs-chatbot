2b7c604a005be55f4c8e48f90e02f71aa9dd0a2d
Edenn Touitou <deozza@gmail.com>
2020-09-29 16:43:01 +0200

    fixed port

--- index.js
+++ index.js
@@ -5,7 +5,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 
 let app = express();
 app.use(bodyParser.json());
